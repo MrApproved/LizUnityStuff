f0733392d23c7bf347cbe00159995f98466bdc10
sdsad <bengwasdasdsaatts@outlook.com>
2020-03-30 18:45:56 +0100

    Added files

---
+++ ExampleStuff/Assets/Scripts/ColourChanging.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChanging : MonoBehaviour
+{
+    // https://stackoverflow.com/questions/34447682/what-is-the-difference-between-update-fixedupdate-in-unity
+    public bool UpdateColourChange;
+    public bool FixedUpdateColourChange;
+
+    private Renderer Renderer { get; set; }
+    private Color CurrentColor { get; set; }
+
+  
+    void Awake()
+    {
+        Renderer = this.gameObject.GetComponent<Renderer>();
+        CurrentColor = new Color(0, 0, 0);
+    }
+
+    void Update()
+    {
+        if (UpdateColourChange)
+        {
+            CurrentColor += new Color(0.01f, 0.01f, 0.01f);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(FixedUpdateColourChange)
+        {
+            CurrentColor += new Color(0.01f, 0.01f, 0.01f);
+        }
+
+        if(CurrentColor.r > 1f || CurrentColor.g > 1f || CurrentColor.b > 1f)
+        {
+            CurrentColor = new Color();
+        }
+
+        Renderer.material.color = CurrentColor;
+    }
+}
---
+++ ExampleStuff/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float MovementModifier;
+    public float JumpForce;
+    private Transform Transform { get; set; }
+    private Rigidbody Rigidbody { get; set; }
+
+    void Awake()
+    {
+        Transform = this.gameObject.GetComponent<Transform>();
+        Rigidbody = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Horizontal is part of the default Edit > Project Settings > Input Manager
+        Transform.position += new Vector3(Input.GetAxis("Horizontal") * MovementModifier, 0, 0);
+        Transform.position += new Vector3(0, 0, Input.GetAxis("Vertical") * MovementModifier);
+
+        // We should do a check here so the player can't spam jump
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody.AddForce(new Vector3(0, JumpForce, 0));
+
+        }
+    }
+}
